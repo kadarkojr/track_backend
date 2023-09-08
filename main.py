b58e8edc33fd056410aa33a130b5d6018b71b36a
kadarkojr <kwabenaamoakodarko47@gmail.com>
2023-09-08 18:33:20 +0000

    stage wai

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from datetime import datetime
 import json
 
 
-weekday = datetime.now().strftime('%A')
-formatted_datetime_str = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
 
 app = FastAPI()
 
 @app.get('/api')
 async def root(slack_name: str = Query(None, description="slack_name"),
                     track: str = Query(None, description="track")):
-    return {
+    weekday = datetime.now().strftime('%A')
+    formatted_datetime_str = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    data_set = {
         "slack_name" : slack_name,
         "current_day" : weekday,
-        "utc_time": formatted_datetime_str,
+        "utc_time": formatted_datetime_str, 
         "track" : track,
         "github_file_url" : "https://github.com/kadarkojr/zuri_backend/blob/main/internship/appp.py",
         "github_repo_url" : "https://github.com/kadarkojr/zuri_backend",
         "status_code" : 200
 
     }
+
+    return data_set
